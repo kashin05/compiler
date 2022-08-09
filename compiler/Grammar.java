51fd13b695050b7d1fb7b1d0216a2cb0d2e827dd
kashin05 <104760248+kashin05@users.noreply.github.com>
2022-08-10 00:31:48 +0800

    加入LR(1)项集族

--- compiler/Grammar.java
+++ compiler/Grammar.java
@@ -1,9 +1,7 @@
 package com.compiler;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * 文法
@@ -12,6 +10,8 @@ import java.util.Set;
  */
 public class Grammar {
 
+    private final AtomicInteger idGen = new AtomicInteger();
+
     /** 开始符号 */
     protected NonTerminal start;
     /** 产生式 */
@@ -19,6 +19,75 @@ public class Grammar {
     /** 所有符号 */
     protected Set<Token> tokens = new HashSet<>();
 
+    /** LR0项集族 */
+    protected Set<SetOfItem> setOfItemsLR0 = new HashSet<>();
+    /** LR1项集族 */
+    protected Set<LR1SetOfItem> setOfItemsLR1 = new HashSet<>();
+
+    /** 项集的GOTO关系 */
+    protected Map<Integer, Map<Token, Integer>> itemGoToItem = new HashMap<>();
+
+    /**
+     * LR0 项集族计算
+     * P157 规范LR(0)项集族的计算
+     * The canonical collection of sets of LR(0) items for an augmented grammar
+     */
+    public void buildLR0SetOfItem() {
+        // 初始化第一个项集
+        SetOfItem first = new SetOfItem();
+        first.id = nextId();
+        first.items = new HashSet<>();
+        List<Production> productionList = getProduction(start);
+        for (Production production : productionList) {
+            Item item = new Item();
+            item.pos = 0;
+            item.production = production;
+            first.items.add(item);
+        }
+        first.closure(this);
+
+        setOfItemsLR0.add(first);
+
+        boolean newAdd;
+        do {
+            newAdd = false;
+
+            Set<SetOfItem> allItemsLR0 = new HashSet<>(setOfItemsLR0);
+            for (SetOfItem setOfItem : allItemsLR0) {
+                for (Token token : tokens) {
+                    SetOfItem setOfItem1 = setOfItem.gotoSetOfItem(this, token);
+                    // GOTO集合为空
+                    if (setOfItem1.items.isEmpty()) {
+                        continue;
+                    }
+
+                    // 判断GOTO集合是否已经存在
+                    SetOfItem old = null;
+                    for (SetOfItem item : setOfItemsLR0) {
+                        if (item.equals(setOfItem1)) {
+                            old = item;
+                            break;
+                        }
+                    }
+
+                    SetOfItem gotoTarget;
+                    if (old != null) {// 已经有相同的项集
+                        gotoTarget = old;
+                    } else {
+                        setOfItem1.id = nextId();
+                        setOfItemsLR0.add(setOfItem1);
+                        newAdd = true;
+                        gotoTarget = setOfItem1;
+                    }
+
+                    // 记录GOTO关系
+                    Map<Token, Integer> gotoSet = itemGoToItem.computeIfAbsent(setOfItem.id, id-> new HashMap<>());
+                    gotoSet.put(token, gotoTarget.id);
+                }
+            }
+        } while (newAdd);
+
+    }
 
     /**
      * P140 FIRST(X)集合
@@ -61,6 +130,9 @@ public class Grammar {
     public Set<Terminal> first(Token token) {
 
         Set<Terminal> result = new HashSet<>();
+
+        // X 是一个终结符号
+        // if X is a terminal, then FIRST(X) = {X}
         if (token instanceof Terminal) {
             result.add((Terminal) token);
             return result;
@@ -72,21 +144,15 @@ public class Grammar {
             // X->ε
             if (production.body.size() == 1 && production.body.get(0).equals(Terminal.of("ε"))) {
                 result.add(Terminal.of("ε"));
+            } else {
+                Set<Terminal> firstSet = first(production.body);
+                result.addAll(firstSet);
             }
-
-            Set<Terminal> firstSet = first(production.body);
-            result.addAll(firstSet);
         }
 
         return result;
     }
 
-    /**
-     * 获得所有head开头的产生式
-     * get productions that start with head
-     * @param head
-     * @return
-     */
     public List<Production> getProduction(NonTerminal head) {
         List<Production> result = new ArrayList<>();
         for (Production production : productionList) {
@@ -97,4 +163,7 @@ public class Grammar {
         return result;
     }
 
+    public int nextId() {
+        return idGen.incrementAndGet();
+    }
 }
